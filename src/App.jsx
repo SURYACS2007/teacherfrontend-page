8799a117247d9c2e741f132e91cba298f60784bb
SURYACS2007 <suryacs572007@gmail.com>
2025-09-17 20:09:51 +0530

    jhgytf

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Student from './Student';
 import CreateStudent from './CreateStudent';
-
 
 import Jpstudent from './Jpstudent';
 import Createjp from './Createjp';
@@ -12,35 +11,26 @@ import Vccfstudent from './Vccffstudent';
 import Createvccf from './Createvccf';
 import Daastudent from './Daastudent';
 import Createdaa from './Createdaa';
-import Createdpco from './Createdpco';
 import Dpcostudent from './Dpcostudent';
-
+import Createdpco from './Createdpco';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="tothepoint_login">
       <Routes>
-        
         <Route path="/" element={<Login />} />
-
-        
         <Route path="/students" element={<Student />} />
         <Route path="/create" element={<CreateStudent />} />
-        <Route path="/jpstudent" element={<Jpstudent/>} />
-        <Route path="/createjp" element={<Createjp/>} />
-        <Route path="/dsstudent" element={<Dsstudent/>} />
-        <Route path="/createds" element={<Createds/>} />
-        <Route path="/vccfstudent" element={<Vccfstudent/>} />
-        <Route path="/createvccf" element={<Createvccf/>} />
-        <Route path="/daastudent" element={<Daastudent/>} />
-        <Route path="/createdaa" element={<Createdaa/>} />
-        <Route path="/dpcostudent" element={<Dpcostudent/>} />
-        <Route path="/createdpco" element={<Createdpco/>} />
-        <Router basename='tothepoint_login'/>
-
-
-
-
+        <Route path="/jpstudent" element={<Jpstudent />} />
+        <Route path="/createjp" element={<Createjp />} />
+        <Route path="/dsstudent" element={<Dsstudent />} />
+        <Route path="/createds" element={<Createds />} />
+        <Route path="/vccfstudent" element={<Vccfstudent />} />
+        <Route path="/createvccf" element={<Createvccf />} />
+        <Route path="/daastudent" element={<Daastudent />} />
+        <Route path="/createdaa" element={<Createdaa />} />
+        <Route path="/dpcostudent" element={<Dpcostudent />} />
+        <Route path="/createdpco" element={<Createdpco />} />
       </Routes>
     </BrowserRouter>
   );
